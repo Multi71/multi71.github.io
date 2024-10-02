b031c13fb5b874f5db63bd02d86e72adf8de5b20
Multi71 <naticks.dark178@gmail.com>
2024-10-02 11:16:36 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/*
+Each API call costs 5 units!
+Single key can refresh the wall every 45 seconds, while 9 keys are needed for a refresh rate 
+of 5 seconds!
+*/
+
+var APIKeys = ["AIzaSyAg3DYLD9tHt4vGhQg68cWLVAo1G6qz-q0"];
+
+var channelcount =23;
+
+//Default Name
+var chanName = "ITZ ZOZO";
+
+//Background Color
+var bgColor = "rgba(255, 255, 255,)";
+
+//Default Image url 
+var imgUrl = "https://yt3.ggpht.com/a/AATXAJxnkPUfl5SntbmDn7XaRsxRHfMxhU_uYXpO2hn62w=s88-c-k-c0x00ffffff-no-rj";
+
+$('body').css("background",bgColor);
---
+++ input.php
@@ -0,0 +1,47 @@
+﻿﻿﻿﻿<?php
+
+$no_of_channels = 18;
+
+$myFile = "channels.json";
+ 
+$arr_data = array(); // create empty array
+
+  try
+  {
+       $id = $_GET['id']; //get ID from the url
+        
+     $jsondata = file_get_contents($myFile); //Get data from existing json
+
+     $arr_data = json_decode($jsondata, true); //converts json data into array
+     
+     function searchForId($arr_data, $id) {
+                foreach ($arr_data as $value) {
+                  if ($value == $id) {
+                  return true;
+                 }
+           }
+   return false;
+   }
+
+  $check = searchForId($arr_data, $id);
+  
+  if ($check == false){
+          if (count($arr_data) > $no_of_channels-1) {
+          array_shift($arr_data);
+          }
+      array_push($arr_data,$id); 
+        $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
+    if (file_put_contents($myFile, $jsondata)) {
+            echo "Seu contador foi adicionado com sucesso à parede "; //Nightbot Reply for successful Addition 
+        } 
+    } 
+
+    else {
+    echo "Acho que seu medidor já está na parede
+."; //Nightbot Reply if duplicates are found
+    }
+    
+   }
+   catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+   }
